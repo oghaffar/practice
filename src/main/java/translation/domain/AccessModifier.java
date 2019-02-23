b1c38e202fd1e9dc2efa8c56c67511aa60f0e313
oghaffar <oghaffar@gmail.com>
2019-02-22 20:08:34 -0600

    Added Unit Test for AccessModifier class

--- src/main/java/translation/domain/AccessModifier.java
+++ src/main/java/translation/domain/AccessModifier.java
@@ -3,34 +3,34 @@ package translation.domain;
 public enum AccessModifier {
     P("Public"), X("Private"), V("View"), I("Inherit");
 
-    private final String expandedValue;
+    private final String longName;
 
-    AccessModifier(String expandedValue) {
-        this.expandedValue = expandedValue;
+    AccessModifier(String longName) {
+        this.longName = longName;
     }
 
-    public String toExpandedValue(final String contractedValue) {
+    public static String toLongName(final String shortName) {
         try {
-            return AccessModifier.valueOf(contractedValue.toUpperCase()).expandedValue;
+            return AccessModifier.valueOf(shortName.toUpperCase()).longName;
         } catch (NullPointerException npe) {
-            throw new RuntimeException("Contracted Value is NULL!");
+            throw new RuntimeException("shortName is NULL!");
         } catch (IllegalArgumentException iae) {
-            throw new RuntimeException(contractedValue + " is not a valid modifier");
+            throw new RuntimeException("'" + shortName + "' is an invalid access modifier!");
         }
     }
 
-    public String toContractedValue(final String expandedValue) {
-        if (expandedValue == null) {
-            throw new RuntimeException("Expanded Value is NULL!");
+    public static String toShortName(final String longName) {
+        if (longName == null) {
+            throw new RuntimeException("longName is NULL!");
         }
 
-        switch (expandedValue.toUpperCase()) {
+        switch (longName.toUpperCase()) {
             case "PUBLIC": return P.name();
             case "PRIVATE": return X.name();
             case "VIEW": return V.name();
             case "INHERIT": return I.name();
             default:
-                throw new RuntimeException(expandedValue + " is an Invalid Access Modifier!");
+                throw new RuntimeException("'" + longName + "' is an invalid access modifier!");
         }
     }
 
--- src/test/java/translation/domain/AccessModifierTest.java
+++ src/test/java/translation/domain/AccessModifierTest.java
@@ -1,9 +1,69 @@
 package translation.domain;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+import static translation.domain.AccessModifier.*;
 
 public class AccessModifierTest {
 
+    @Test
+    public void testTranslationFromShortToLongName() {
+        assertEquals("Public", toLongName("P"));
+        assertEquals("Public", toLongName("p"));
+        assertEquals("Private", toLongName("X"));
+        assertEquals("Private", toLongName("x"));
+        assertEquals("View", toLongName("V"));
+        assertEquals("View", toLongName("v"));
+        assertEquals("Inherit", toLongName("I"));
+        assertEquals("Inherit", toLongName("i"));
+    }
 
+    @Test
+    public void testTranslationFromLongToShortName() {
+        assertEquals("P", toShortName("PuBliC"));
+        assertEquals("X", toShortName("privATE"));
+        assertEquals("V", toShortName("VieW"));
+        assertEquals("I", toShortName("inherit"));
+    }
 
+    @Test
+    public void testCallingToLongNameWithNullThrowsException() {
+        try {
+            toLongName(null);
+            fail("Expected runtime exception!");
+        } catch(RuntimeException e) {
+            assertEquals("shortName is NULL!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCallingtoShortNameWithNullThrowsException() {
+        try {
+            toShortName(null);
+            fail("Expected runtime exception!");
+        } catch(RuntimeException e) {
+            assertEquals("longName is NULL!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCallingToLongNameWithInvalidValueThrowsException() {
+        try {
+            toLongName("Invalid Value");
+            fail("Expected runtime exception!");
+        } catch(RuntimeException e) {
+            assertEquals("'Invalid Value' is an invalid access modifier!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCallingToShortNameWithInvalidValueThrowsException() {
+        try {
+            toShortName("Q");
+            fail("Expected runtime exception!");
+        } catch(RuntimeException e) {
+            assertEquals("'Q' is an invalid access modifier!", e.getMessage());
+        }
+    }
 }
