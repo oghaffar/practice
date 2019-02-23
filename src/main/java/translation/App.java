5197035b82354d5a04602b94fab36689e3f2247a
oghaffar <oghaffar@gmail.com>
2019-02-22 20:25:37 -0600

    Added routes

--- src/main/java/translation/App.java
+++ src/main/java/translation/App.java
@@ -1,11 +1,29 @@
 package translation;
 
+import translation.domain.AccessModifier;
+
 import static spark.Spark.get;
 
 public class App {
 
     public App() {
-        get("/hello", (req, res) -> " Something! ");
+        get("translate/toLongName", (req, res) -> {
+            String shortName = req.queryParams("shortName");
+            try {
+                return AccessModifier.toLongName(shortName);
+            } catch (Exception e) {
+                return e.getMessage();
+            }
+        });
+
+        get("translate/toShortName", (req, res) -> {
+            String longName = req.queryParams("longName");
+            try {
+                return AccessModifier.toShortName(longName);
+            } catch (Exception e) {
+                return e.getMessage();
+            }
+        });
     }
 
     public static void main(String[] args) {
