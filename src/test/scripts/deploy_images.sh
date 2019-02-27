3a02d62f97cef3ab878a164d64e75acb8405965e
oghaffar <oghaffar@gmail.com>
2019-02-27 15:56:40 -0600

    Cleaned up deploy script

--- src/test/scripts/deploy_images.sh
+++ src/test/scripts/deploy_images.sh
@@ -3,33 +3,15 @@
 stackName=practiceIT
 expectedReplicas=9
 
-#DOCKER_HOST=tcp://192.168.206.130:2375
-#docker container rm --force $(docker container ls -q)
-#docker image rm --force $(docker image ls -q)
-#mvn dockerfile:build &
-#
-#mvn1pid=$!
-#
-#DOCKER_HOST=tcp://192.168.206.129:2375
-#docker container rm --force $(docker container ls -q)
-#docker image rm --force $(docker image ls -q)
-#mvn dockerfile:build &
-#
-#mvn2pid=$!
+#docker container kill $(docker container ls | grep postgres | cut -c -12)
 
-#DOCKER_HOST=tcp://192.168.206.128:2375
 docker container rm --force $(docker container ls -q)
-docker image rm --force $(docker image ls -q)
+docker image rm -v --force $(docker image ls -q)
 mvn dockerfile:build
-
-#mvn3pid=$!
 
 docker stack rm ${stackName} &
 
 wait $!
-#wait $mvn1pid
-#wait $mvn2pid
-#wait $mvn3pid
 
 docker stack deploy -c docker-compose.yml ${stackName}
 
@@ -39,7 +21,7 @@ do
     sleep 2
 done
 
-sleep 5   # needed because db is not responsive right after it has been started
+sleep 1   # this sadness is needed because db is not responsive right after it has been started
 
 
 
